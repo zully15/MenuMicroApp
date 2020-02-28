fd270dbf4b6d95a2bf014ed013e9b68fd186ce75
Sizi B Rios <sr73238@srcs.loc>
2020-02-28 14:45:16 -0800

    e

--- Dinner.js
+++ Dinner.js
@@ -1,17 +1,19 @@
-var cimg =  document.createElement("IMG");
-var fimg = document.createElement("IMG");
+var cimg = document.createElement("IMG");
+
 
 let button = document.getElementById("my_button")
-button.addEventlistener("click" , () =>){
+button.addEventlistener("click", () => ) {
 
    var  imgs  =     [ "https://pngimg.com/uploads/steak/steak_PNG66.png", "https://ya-webdesign.com/transparent250_/dinner-fish-dishes-vector-png-3.png", "https://ya-webdesign.com/transparent250_/pizza-clipart-margherita-pizza-19.png", "https://ya-webdesign.com/transparent250_/vector-hamburger-pizza-4.png","https://ya-webdesign.com/transparent250_/sushi-plate-png-6.png", "https://ya-webdesign.com/transparent250_/sushi-png-3.png", "https://ya-webdesign.com/transparent250_/bowl-of-soup-png-1.png", "https://ya-webdesign.com/transparent250_/chicken-soup-png-8.png", "https://ya-webdesign.com/transparent250_/mexican-food-png-6.png", "https://ya-webdesign.com/transparent250_/mexican-food-png-7.png", "https://ya-webdesign.com/transparent250_/mexican-food-png-5.png", ]
 
 
-    var random = Math.floor(Math.random() * 11)
-     img.setAttribute("src", imgs[random])
+
+    var random = Math.floor(Math.random() * 9)
+     cimg.setAttribute("src", imgs[random])
      document.body.appendChild(cimg)
 
 
 
 
                         })
+
